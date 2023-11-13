fb29e899a436204cac371564a2415862b5ddf2b6
Braingearceo <104493450+Braingearceo@users.noreply.github.com>
2023-11-13 15:06:55 +0100

    Update youai_youtube_transcript.py

--- youai_youtube_transcript.py
+++ youai_youtube_transcript.py
@@ -13,3 +13,29 @@ async def get_transcript(video_id: str):
 
     full_text = " ".join([entry['text'] for entry in transcript])
     return {"video_id": video_id, "transcript": full_text}
+
+@app.get("/privacy-policy", response_class=PlainTextResponse)
+async def get_privacy_policy():
+    privacy_policy = """
+    Privacy Policy for youtube-transcript
+
+    Welcome toyoutube-transcript. We are committed to protecting your privacy. This Privacy Policy explains how we collect, use, and disclose information through our application youtube-transcript.
+    
+    Information We Collect
+    User-Provided Information: When you use our service to obtain transcripts from YouTube videos, you provide us with the video ID. We do not require you to create an account, nor do we collect any personal information.
+    
+    Automatically Collected Information: Our application does not automatically collect any personal information about you. We may collect non-personal information about your use of the app, such as error logs, for the purpose of improving our service.
+    
+    How We Use Information
+    We use the video ID provided by you solely for the purpose of fetching the corresponding transcript from YouTube. We do not store, share, or use this information for any other purposes.
+    
+    Data Storage and Security
+    We do not store any personal data or transcripts obtained during your use of our service. Our application is designed to ensure that your use of the service remains private and secure.
+    
+    Changes to This Privacy Policy
+    We may update this Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page. You are advised to review this Privacy Policy periodically for any changes.
+    
+    Contact Us
+    If you have any questions about this Privacy Policy, please contact us at kevinlopez@braincorp.fr.
+    """
+    return privacy_policy
